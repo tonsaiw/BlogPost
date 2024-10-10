d3caafc9ef8d27cce7a3a967c4dbfba858cf54af
tonsaiw <tonsai10538@gmail.com>
2024-10-10 11:38:44 +0700

    update app module and main.ts for use cookie

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,7 +4,11 @@ import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { CommentsModule } from './comments/comments.module';
 
+// Set up the ConfigModule and MongooseModule
 
 @Module({
   imports: [ ConfigModule.forRoot({
@@ -20,7 +24,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         pass: configService.get<string>('MONGO_PASSWORD'),
         dbName: configService.get<string>('MONGO_DB_NAME'),
       }),
-    }), PostsModule],
+    }), PostsModule, UserModule, AuthModule, CommentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
