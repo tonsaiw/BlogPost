c5c2749c30448c88fc5b2041ffc73b0069e32a7d
tonsaiw <tonsai10538@gmail.com>
2024-10-10 11:46:56 +0700

    update package json library axios and react-query

--- frontend/src/app/blog/page.tsx
+++ frontend/src/app/blog/page.tsx
@@ -1,5 +1,5 @@
 import { Header } from "../components/common/Header";
-import { Sidebar } from "../components/common/SideBar";
+import { Sidebar } from "../components/common/Sidebar";
 const BlogPage = () => {
   return (
     <div>
